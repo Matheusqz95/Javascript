ef375e5fb079b7d028e1cac688c3bbadc9930133
MatheusQueiroz95 <matheusaparecido253@hotmail.com>
2023-01-20 10:32:38 -0300

    ATT

---
+++ aula13/node_dd9e074f9208f.tmp.js
@@ -0,0 +1,29 @@
+var c = 1
+while ( c < 6) {
+    console.log('tudo bem') 
+}
+
+
+
+
+
+
+
+
+
+/*
+
+var c = 1
+while ( c < 6) {
+    console.log('tudo bem') 
+}
+
+
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
