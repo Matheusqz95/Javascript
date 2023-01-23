ff88d4aaaf668e637a477d48d247c39336c11fae
MatheusQueiroz95 <matheusaparecido253@hotmail.com>
2023-01-23 11:58:23 -0300

    Usando FOR(inicialização;TESTE;incrementação){}

--- aula14/ex14.js
+++ aula14/ex14.js
@@ -1,9 +1,17 @@
-/*for (var c = 1; c <= 10; c++){
-    console.log(c)
-}
-*/
-var c = 1
-while (c <= 10){
+
+/*var c = 1
+while (c >= 10){
 console.log(c)
 c++
 }
+
+for (var c = 100; c >= 1; c--;) {
+    console.log(c)
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(c)
+} 
+console.log('FIM') 
